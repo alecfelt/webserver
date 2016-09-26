9f8e2b5b33630d43f020d2d147723f6ee9d733c2
Alec Felt <allfelt@ucsc.edu>
2016-09-25 17:34:41 -0700

    set up server and WSGI and 2 web frameworks

--- webserver.py
+++ webserver.py
@@ -7,15 +7,18 @@ listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print 'Serving HTTP on port %s ...' % PORT
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print request
+client_connection, client_address = listen_socket.accept()
+request = client_connection.recv(1024)
+print request
 
-    http_response = """\
-HTTP/1.1 200 OK
+http_response = """HTTP/1.1 200 OK
+Content-type: text/html
 
-Hello, World!
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
+
+<html>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+</html>"""
+client_connection.sendall(http_response)
+client_connection.close()
